bcf0277421787d0dad863e49f1e9e88d98a55f22
bszeidl <szeidl.balazs@hotmail.com>
2022-12-25 19:40:37 +0100

    3 triangles are ready.

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+    
+    
+    
+    
+    function triangle (positionX, positionY, size) {
+        context.beginPath();
+        context.moveTo(positionX,positionY);
+        context.lineTo(positionX + size * .5,positionY + size);
+        context.lineTo(positionX - size * .5,positionY + size);
+        context.lineTo(positionX,positionY);
+        
+        
+        context.fillStyle = 'rgba(255,165,0,.5)';
+        context.fill();
+        context.strokeStyle = 'rgba(0,0,0,.5)';
+        context.stroke();
+
+
+    }    
+    
+    
+    triangle(230, 160, 50);
+    triangle(270, 100, 50);
+    triangle(200, 50, 150);
+
+    
+    
+
 
 }
 
